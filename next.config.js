a270ef3d346c1106b71ab0251e19931323701094
nthdat2704 <nthdat2704@gmail.com>
2023-09-17 23:43:18 +0700

    setup source

--- next.config.js
+++ next.config.js
@@ -1,9 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  sassOptions: {
-    additionalData: `@import "src/styles/variables.scss"; @import "src/styles/variables.scss";`,
-  },
+    reactStrictMode: true,
+    sassOptions: {
+        additionalData: `@import "src/styles/variables.scss"; @import "src/styles/variables.scss";`,
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: '**',
+                port: '',
+                pathname: '**',
+            },
+        ],
+    },
 };
 
 module.exports = nextConfig;
