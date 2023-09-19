3dacc178ad256a763c4758f3e5d7dfd05d6cfe25
nthdat2704 <nthdat2704@gmail.com>
2023-09-19 12:59:23 +0700

    add images assets

---
+++ src/shared/assets/images/index.ts
@@ -0,0 +1,35 @@
+import animal1 from './animal-1.png'
+import animal10 from './animal-10.png'
+import animal11 from './animal-11.png'
+import animal12 from './animal-12.png'
+import animal13 from './animal-13.png'
+import animal14 from './animal-14.png'
+import animal15 from './animal-15.png'
+import animal16 from './animal-16.png'
+import animal2 from './animal-2.png'
+import animal3 from './animal-3.png'
+import animal4 from './animal-4.png'
+import animal5 from './animal-5.png'
+import animal6 from './animal-6.png'
+import animal7 from './animal-7.png'
+import animal8 from './animal-8.png'
+import animal9 from './animal-9.png'
+import bannerOneMoreHuman from './banner-one-more-human.png'
+import bannerOneMore from './banner-one-more.png'
+import brand1 from './brand-1.png'
+import brand2 from './brand-2.png'
+import brand3 from './brand-3.png'
+import brand4 from './brand-4.png'
+import brand5 from './brand-5.png'
+import brand6 from './brand-6.png'
+import brand7 from './brand-7.png'
+import hand from './hand.png'
+import bannerPet from './banner-pet-seller.png'
+
+
+export {
+    animal1, animal10, animal11,
+    animal12, animal13, animal14, animal15,
+    animal16, animal2, animal3, animal4, animal5,
+    animal6, animal7, animal8, animal9, bannerOneMore, bannerOneMoreHuman, brand1, brand2, brand3, brand4, brand5, brand6, brand7, hand, bannerPet
+}
